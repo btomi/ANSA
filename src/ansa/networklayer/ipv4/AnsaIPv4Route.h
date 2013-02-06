6dc49061bdff263ffb22f0fb12c0b3a6a96d7285
tomyx <xproch21@stud.fit.vutbr.cz>
2013-02-06 21:59:58 +0100

    Adding copyright to PIM-DM and ANSA networklayer

--- src/ansa/networklayer/ipv4/AnsaIPv4Route.h
+++ src/ansa/networklayer/ipv4/AnsaIPv4Route.h
@@ -1,7 +1,24 @@
+// Copyright (C) 2013 Brno University of Technology (http://nes.fit.vutbr.cz/ansa)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
 /**
- * @file AnsaIPv4Route.h
+ * @file AnsaIPv4Route.cc
  * @date 25.1.2013
- * @author: Veronika Rybova, Tomas Prochazka
+ * @author Veronika Rybova, Tomas Prochazka (mailto:xproch21@stud.fit.vutbr.cz),
+ * Vladimir Vesely (mailto:ivesely@fit.vutbr.cz)
+ * @brief Inherited class from IPv4Route for PIM purposes
+ * @detail File implements new functions for IPv4Route which are needed for PIM
  */
 
 #ifndef ANSAIPV4ROUTE_H_
